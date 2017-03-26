601f2b836730a34bc691f57815faa81baeb58305
Stepan <creadtive472@gmail.com>
2017-03-26 20:19:22 +1000

    findSimilarities

---
+++ src/background/compare.js
@@ -0,0 +1,13 @@
+;var NECTARINE = (function (myModel) {
+    console.log("NECTARINE ==> compare.js");
+    
+    "use strict";
+    
+
+    myModel.findSimilarities = function(){
+        
+    };
+    
+    return myModel;
+    
+}(MANDARIN || {}));
--- src/background/history.js
+++ src/background/history.js
@@ -1,24 +1,13 @@
 
-var setting = [
-    /google/i,
-    /habrahabr/i,
-    /kinogo/i
-];
-
-var setting_lenght = setting.length - 1;
+//var setting = [
+//    /google/i,
+//    /habrahabr/i,
+//    /kinogo/i
+//];
+//
+//var setting_lenght = setting.length - 1;
 
 chrome.history.onVisited.addListener(function(results){
    
-    var URR = results.url;
-    
-    for (var i = 0; i <= setting_lenght; i++) {
-        if(setting[i].test(URR)){
-            console.log("sendMesage.js ➤ TRUE ➤ " + URR);
-            chrome.history.deleteUrl({url: URR}, function(){
-                console.info("sendMesage.js ➤ DELETE TRUE ➤ ", URR);
-            });
-        }else{
-            console.log("sendMesage.js ➤ FALSE ➤ onMessage ➤" + URR);
-        }
-    }
+    console.log(results);
 })
