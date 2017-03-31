8ac5a38c32905f51fc5499125d50b1e125a0fbab
Stepan <creadtive472@gmail.com>
2017-03-31 14:26:25 +1000

    htmlparse

--- src/background/compare.js
+++ src/background/compare.js
@@ -3,7 +3,17 @@
     
     "use strict";
     
-
+    /**
+     * findSimilaritiesURL
+     *
+     * NECTARINE.findSimilaritiesURL("https://www.google.ru/", ["google", "kinogo"])
+     *
+     * Поиск значения в URL страници
+     * @param {URL}  String Адрес страници
+     * @param {regx} Arrya Массив для сравнения в адресе страници 
+     * @return [bool, URL]
+    */
+    
     myModel.findSimilaritiesURL = function(URL, regx){
     
         regx = new RegExp(regx.join('|'), "i");
@@ -15,12 +25,30 @@
         }
     };
     
+    /**
+     * findSimilaritiesHTML
+     *
+     * NECTARINE.findSimilaritiesHTML("<h1>google</h1>", ["google", "kinogo"])
+     * 
+     * Поиск значения в HTML страници
+     * @param {HTML} String Текст страници
+     * @param {regx} Arrya Массив для сравнения текста страници 
+     * @return bool
+    */
+    
     myModel.findSimilaritiesHTML = function(HTML, regx){
-        return true;
+        
+        HTML = HTML.replace(/\s{2,}/g, '');
+
+        regx = new RegExp(regx.join('|'), "ig");
+        
+        if(regx.test(HTML)){
+            return true;
+        }
+        
+        return false;
     };
     
     return myModel;
     
 }(NECTARINE || {}));
-
-//NECTARINE.findSimilaritiesURL("https://www.google.ru/", ["google", "habrahabr", "kinogo"])
