b403c1749d67b1d635a0db9ac5f5a651ba0a97d3
Stepan <creadtive472@gmail.com>
2017-04-08 16:50:39 +1000

    addHTMLStorage(filterMass)

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,6 +2,11 @@
     
     $(document).ready(function(){
         
+        CHROMESTO.getStorage("history", function(page){
+            addHTMLStorage(page)
+        });
+        
+        
         $(".gala27").click(function() {
             
             var valueFilter = $(".gala12").val();
@@ -12,27 +17,37 @@
                 return
             }
             
-            CHROMESTO.getStorage("history", function(page){
-                
-                //console.log(page)
-                
-                if(!page){
-                    CHROMESTO.setStorage({"history": [valueFilter]}, function(){});
-                    return
-                }
-                
-                page[page.length] = valueFilter;
-                
-                //console.log(page)
-                
-                CHROMESTO.setStorage({"history": page}, function(){});
-                
-            });
+            addParametrRegx(valueFilter);
             
         });
         
     });
+    
+    function addParametrRegx(valueFilter){
+        CHROMESTO.getStorage("history", function(page){
+                
+            //console.log(page)
+            
+            if(!page){
+                CHROMESTO.setStorage({"history": [valueFilter]}, function(){});
+                return
+            }
+            
+            page[page.length] = valueFilter;
+            
+            //console.log(page)
+            
+            CHROMESTO.setStorage({"history": page}, function(){});
+                
+        });
+    };
+    
+    function addHTMLStorage(filterMass){
+        $.each(filterMass, function() {
+            $('<li/>', {
+                text: DECODE.utf8ToB64(this)
+            }).appendTo($(".border"));
+        });
+    }
 
 }(jQuery))
-
-//page[page.length] = 'Компьютер'
