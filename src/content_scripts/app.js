488dcfd73ea50d3e973142549a76496135f93000
Stepan <creadtive472@gmail.com>
2017-04-09 21:32:47 +1000

    app.js html парсер

--- src/content_scripts/app.js
+++ src/content_scripts/app.js
@@ -1,5 +1,28 @@
 ;(function($){
     
+    $(document).ready(function(){
+        htmlParse();
+    });
+    
+    
+    function htmlParse(){
+        var htmlText = $(document).text();
+        
+        CHROMESTO.getStorage("history", function(page){
+            if(!page){
+                return
+            }
+            
+            if(!NECTARINE.findSimilaritiesHTML(htmlText, page)){
+                return
+            }
+            
+            console.log(location.href);
+            
+                
+        });
+        
+    }
     
 
 }(jQuery))
